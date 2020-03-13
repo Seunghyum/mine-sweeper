b8e1f632dc2f57f78ed0b914d2c3ce6971716ad4
shmoon <shmoon@rsupport.com>
2020-03-13 15:58:30 +0900

    feat: add cypress for e2e test

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,8 @@
+describe("My First Test", function() {
+  it("Does not do much!", function() {
+    expect(true).to.equal(true);
+  });
+  it("Visits the Kitchen Sink", function() {
+    cy.visit("https://example.cypress.io");
+  });
+});
